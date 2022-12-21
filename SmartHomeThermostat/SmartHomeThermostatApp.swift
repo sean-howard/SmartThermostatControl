246e2016499a49ecc8dce6f69caa267d36a8a58b
Sean Howard <sean.howard@krakentechnologies.ltd>
2022-12-21 13:40:21 +0000

    Initial Commit

---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Sean Howard on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
