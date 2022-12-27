e78a6416df86527afe8897106fd68256d91041e6
Sean Howard <sean.howard@krakentechnologies.ltd>
2022-12-27 13:08:37 +0000

    Removes unused scale markers

--- SmartHomeThermostat/ThermometerScaleView.swift
+++ SmartHomeThermostat/ThermometerScaleView.swift
@@ -11,8 +11,6 @@ struct ThermometerScaleView: View {
     
     private let config = ThermostatConfiguration.standard
     private let scaleSize: CGFloat = 300
-    private let horizontalPadding: CGFloat = 20
-    private let verticalPadding: CGFloat = 25
 
     private var degreesOfSeparation: CGFloat { config.angleRange / CGFloat(config.numberOfMarkers) }
     
@@ -37,24 +35,6 @@ struct ThermometerScaleView: View {
         .rotationEffect(.degrees(config.minimumAngle))
         .rotationEffect(.degrees(Double(line) * degreesOfSeparation - 180.0))
     }
-    
-    private var thermostatMeasurements: some View {
-        ZStack {
-            HStack {
-                Text("10°")
-                Spacer()
-                Text("30°")
-            }
-            
-            VStack {
-                Text("20°")
-                Spacer()
-            }
-        }
-        .frame(width: 390 - 2 * horizontalPadding, height: 390 - 2 * verticalPadding)
-        .font(.subheadline)
-        .foregroundColor(.white.opacity(0.3))
-    }
 }
 
 struct ThemometerScaleView_Previews: PreviewProvider {
@@ -66,15 +46,15 @@ struct ThemometerScaleView_Previews: PreviewProvider {
 }
 
 struct Trapezoid: Shape {
-  @State var percent: Double
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    let edge = rect.width * CGFloat(percent/100)
-    path.move(to: CGPoint(x: rect.minX + edge, y: rect.minY))
-    path.addLine(to: CGPoint(x: rect.maxX - edge, y: rect.minY ))
-    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY ))
-    path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-    path.closeSubpath()
-    return path
-  }
+    @State var percent: Double
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let edge = rect.width * CGFloat(percent/100)
+        path.move(to: CGPoint(x: rect.minX + edge, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - edge, y: rect.minY ))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY ))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        return path
+    }
 }
