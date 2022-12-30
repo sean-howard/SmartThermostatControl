e69cc43a0fa36f794648af4b2486071941b0dd11
Sean Howard <sean.howard@krakentechnologies.ltd>
2022-12-30 11:16:34 +0000

    Moves scale marker to its own file

--- SmartHomeThermostat/ThermometerScaleView.swift
+++ SmartHomeThermostat/ThermometerScaleView.swift
@@ -79,31 +79,3 @@ struct ThemometerScaleView_Previews: PreviewProvider {
             .background(Color("Background"))
     }
 }
-
-struct ScaleMarker: View {
-    
-    @Binding var fillColor: Color
-    
-    var body: some View {
-        Trapezoid(percent: 10)
-            .fill(fillColor)
-            .frame(width: 6, height: 22)
-            .rotationEffect(.degrees(180))
-            .cornerRadius(20)
-            .animation(.linear(duration: 1), value: fillColor)
-    }
-}
-
-struct Trapezoid: Shape {
-    @State var percent: Double
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let edge = rect.width * CGFloat(percent/100)
-        path.move(to: CGPoint(x: rect.minX + edge, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - edge, y: rect.minY ))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY ))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.closeSubpath()
-        return path
-    }
-}
